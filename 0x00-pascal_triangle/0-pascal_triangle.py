f55ea4cd8ba426ed2c73bfe65e9c7fc6d1070d14
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-10 17:26:41 +0100

    resolve pycodestyle issues

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,8 @@
 Contains a function that implement pascal triangle
 algorithm
 """
+
+
 def pascal_triangle(n):
     """
     return a list of lists of integers representing the
@@ -19,17 +21,15 @@ def pascal_triangle(n):
         new_arr = []
         new_arr.append(1)
         for j in range(len(prev_arr)):
-             if ((j + 1) >= len(prev_arr)):
+            if ((j + 1) >= len(prev_arr)):
                 break
 
-             first =prev_arr[j]
-             second = prev_arr[j + 1]
-             new_arr.append(first + second)
+            first = prev_arr[j]
+            second = prev_arr[j + 1]
+            new_arr.append(first + second)
 
         new_arr.append(1)
         prev_arr = new_arr.copy()
         pascal_arr.append(prev_arr)
-    
-    return pascal_arr
 
-        
+    return pascal_arr

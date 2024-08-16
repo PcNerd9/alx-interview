a65388ea9d4429dd99c92c80441a014e4e8fbfd9
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-16 21:34:02 +0100

    resolve the bugs in the 0-lockboxes.py file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,15 +10,22 @@ def canUnlockAll(boxes):
     return true if all the boxes can be unlocked
     otherwise return false
     """
-
-    queue = [0] + boxes[0]
-    i = 0
-
+    if len(boxes) == 1:
+        return True
+    filter_box = [key for key in boxes[0] if key < len(boxes)]
+    unique_filter_key = [0]
+    for key in filter_box:
+        if key not in unique_filter_key:
+            unique_filter_key.append(key)
+    queue = unique_filter_key
+    i = 1
+    
     while i < len(queue):
-        box = boxes[queue[i]]
-        for key in box:
-            if key not in queue:
-                queue.append(key)
+        if queue[i] < len(boxes):
+            box = boxes[queue[i]]
+            for key in box:
+                if key not in queue and key < len(boxes):
+                    queue.append(key)
         i += 1
     if len(queue) == len(boxes):
         return True

fbbeeac32d78f792b65ad63a44094647d44ad994
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-08-16 22:06:40 +0100

    resolve requirement issues

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -12,14 +12,20 @@ def canUnlockAll(boxes):
     """
     if len(boxes) == 1:
         return True
+
+    # filter the keys in the box that it only contain
+    # only the keys for the boxes
     filter_box = [key for key in boxes[0] if key < len(boxes)]
+
+    # make sure that the keys are unique
     unique_filter_key = [0]
     for key in filter_box:
         if key not in unique_filter_key:
             unique_filter_key.append(key)
     queue = unique_filter_key
     i = 1
-    
+
+    # create a queue of keys
     while i < len(queue):
         if queue[i] < len(boxes):
             box = boxes[queue[i]]
@@ -27,6 +33,9 @@ def canUnlockAll(boxes):
                 if key not in queue and key < len(boxes):
                     queue.append(key)
         i += 1
+
+    # check if the lenght of queue is equal to the length
+    # of the boxes
     if len(queue) == len(boxes):
         return True
     else:

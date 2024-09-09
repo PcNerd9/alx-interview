00267e9b337fff3e183a175a538ed11ced3064ff
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-09-09 05:09:54 +0100

    validate utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,18 +1,25 @@
+#!/usr/bin/python3
+"""
+Definesa UTF-8 validation function
+"""
+
+
 def validUTF8(data):
-    n_bytes = 0  # Number of continuation bytes we expect
-    
-    # Masks for determining how many bytes a character occupies
-    byte1_mask = 0b10000000  # Mask for 1-byte characters (0xxxxxxx)
-    byte2_mask = 0b11000000  # Mask for leading byte of 2-byte characters (110xxxxx)
-    byte3_mask = 0b11100000  # Mask for leading byte of 3-byte characters (1110xxxx)
-    byte4_mask = 0b11110000  # Mask for leading byte of 4-byte characters (11110xxx)
-    cont_mask = 0b11000000   # Mask for continuation bytes (10xxxxxx)
-    
+    """
+    UTF-8 Validation function
+    """
+    n_bytes = 0
+
+    byte1_mask = 0b10000000
+    byte2_mask = 0b11000000
+    byte3_mask = 0b11100000
+    byte4_mask = 0b11110000
+    cont_mask = 0b11000000
+
     for byte in data:
-        byte = byte & 0xFF  # Only consider the last 8 bits of the integer (as 1 byte)
-        
-        if n_bytes == 0:  # If we are not expecting any continuation bytes
-            # Determine how many bytes the character should take up based on the first byte
+        byte = byte & 0xFF
+
+        if n_bytes == 0:
             if byte & byte1_mask == 0:
                 # 1-byte character, ASCII range (0xxxxxxx)
                 continue
@@ -31,10 +38,7 @@ def validUTF8(data):
         else:
             # If we are expecting continuation bytes
             if byte & cont_mask != byte1_mask:
-                # If the byte doesn't match the continuation byte pattern (10xxxxxx), it's invalid
                 return False
             n_bytes -= 1  # One less continuation byte to expect
-    
-    # If we finish and still expect more continuation bytes, the input is invalid
-    return n_bytes == 0
 
+    return n_bytes == 0

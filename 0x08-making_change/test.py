0d6a8fa2cc26993cf686258e326f34de2a29dd50
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-11-29 02:35:06 +0100

    tried another approach

--- 0x08-making_change/test.py
+++ 0x08-making_change/test.py
@@ -10,18 +10,15 @@ def makeChange(coins, total):
     Compute the minimum number of coins required to make up a given
     total amount, given a list of coins denominations
     """
-
     if total <= 0:
         return 0
-    # sort the coins in descending order
-    coins.sort(reverse=True)
-    change = 0
+
+    dp = [float("inf")] * (total + 1)
+
+    dp[0] = 0
+
     for coin in coins:
-        if total <= 0:
-            break
-        temp = total // coin
-        change += temp
-        total -= (temp * coin)
-    if total != 0:
-        return -1
-    return change
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float("inf") else -1

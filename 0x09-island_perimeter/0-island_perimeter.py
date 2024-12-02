c4a8428214a2c0186aff36910b8f48b0bb7e8465
Habeeb Ajayi <Ajayihabeeb977@gmail.com>
2024-12-02 23:03:30 +0100

    rewrote the island perimeter algorithm"

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 
 """
-contain a function island_perimeter
+Performs the island perimeter algorithm
 """
 
 
-def island_perimeter(grid) -> int:
+def island_perimeter(grid):
     """
-    compute the island perimiter of a list of list represented
-    as grid where 1 represent a land and 0 represent water
+    compute the island perimeter
     """
-
     perimeter = 0
+    border = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                perimeter += 4
-
-                if j - 1 >= 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
-
-                if j + 1 < len(grid[0]) and grid[i][j + 1] == 1:
-                    perimeter -= 1
-
-                if i - 1 >= 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1
-
-                if i + 1 < len(grid) and grid[i + 1][j] == 1:
-                    perimeter -= 1
+                border = 4
+                if j != 0:
+                    if grid[i][j - 1] == 1:
+                        border -= 1
+                if j != (len(grid[0]) - 1):
+                    if grid[i][j + 1] == 1:
+                        border -= 1
+                if i != 0:
+                    if grid[i - 1][j] == 1:
+                        border -= 1
+                if i != (len(grid) - 1):
+                    if grid[i + 1][j] == 1:
+                        border -= 1
+                perimeter += border
     return perimeter
---
+++ 0x09-island_perimeter/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+contain a function island_perimeter
+"""
+
+
+def island_perimeter(grid) -> int:
+    """
+    compute the island perimiter of a list of list represented
+    as grid where 1 represent a land and 0 represent water
+    """
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if j - 1 >= 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+
+                if j + 1 < len(grid[0]) and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+                if i - 1 >= 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1
+
+                if i + 1 < len(grid) and grid[i + 1][j] == 1:
+                    perimeter -= 1
+    return perimeter
